8c08e682a281f95041fccc15be3690231c1e33a0
RSA-ARG\mveron <matias.veronperalta@segurossura.com.ar>
2020-09-08 16:18:17 -0300

    Generate list categories con GifExpertApps.js

--- 05-gif-expert-app/src/GifExpertApp.js
+++ 05-gif-expert-app/src/GifExpertApp.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One punch', 'Dragon Ball', 'Naruto'])
+    
+    const handleAdd = () => {
+        setCategories([...categories, 'Holi']);
+    };
+
     return ( 
         <>
             <h2>GifExpertApp</h2>
             <hr />
+            <button onClick={ handleAdd }> Agregar Categoría </button>
+            <ol>
+                {
+                    categories.map(category =>{
+                        return <li key={category}> {category} </li>
+                    })
+                }
+            </ol>
         </>
      );
 }
