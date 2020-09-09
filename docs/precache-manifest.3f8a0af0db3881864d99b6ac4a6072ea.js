896c0add7b81df532592e39d6e8fba8ef088c52c
Matias Veron Peralta <matias.veronperalta@segurossura.com.ar>
2020-09-09 14:30:22 -0300

    global docs commit

---
+++ docs/precache-manifest.3f8a0af0db3881864d99b6ac4a6072ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a845f16517de98e065b9d417aa18c469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9b93b017b66f75a6f4b",
+    "url": "/static/css/main.5b2508b1.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9b93b017b66f75a6f4b",
+    "url": "/static/js/main.dfaa9a2e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
