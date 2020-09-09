eb90f070a7b0629d6ee8fcb9cba44a4b0a60cd15
RSA-ARG\mveron <matias.veronperalta@segurossura.com.ar>
2020-09-09 10:54:01 -0300

    Add image gifs list

--- 05-gif-expert-app/src/components/GifGrid.js
+++ 05-gif-expert-app/src/components/GifGrid.js
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
-export const GifGrid = ({category}) => {
+export const GifGrid = ({ category }) => {
 
-    const getGifs = async() =>{
+    const [images, setImages] = useState([])
+
+    useEffect(() => {
+        getGifs();
+    }, []);
+
+    const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=rick+and+morty&limit=10&api_key=UkXtWw1t02SZK27KypfrzFJ3wf5sWLnz';
         const response = await fetch(url);
-        const {data} = await response.json();
+        const { data } = await response.json();
 
-        const gifs = data.map( img => {
-            return{
+        const gifs = data.map(img => {
+            return {
                 id: img.id,
                 title: img.title,
                 url: img.images?.downsized_medium.url,
             }
         });
 
-        console.log(gifs);
+        setImages(gifs);
     }
-
-    getGifs();
 
     return (
         <>
             <h3>{category}</h3>
+            {
+                images.map(image => 
+                    <GifGridItem
+                        key={image.id}
+                        {...image}
+                    />
+                )
+            }
         </>
     )
 }
